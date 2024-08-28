13eb8810a2c273cc15448dea595d5e3420ea6a27
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-28 13:23:49 -0400

    created mobile nav bar and side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,30 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedin = { firstName: "Jordan", lastName: "Kelly" };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedin} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            alt="logo"
+          />
+          <div>
+            <MobileNav user={loggedin} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
