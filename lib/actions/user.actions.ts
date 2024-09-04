76c44924b4478e83593806c758e11296336ca978
Jordan Kelly <jkyne6368@yahoo.com>
2024-09-03 20:26:29 -0400

    refactor

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,14 @@
 
 export const signIn = async () => {
   try {
+    // Mutation / Database / Make Fetch Request
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create a new user account
   } catch (error) {
     console.error("Error", error);
   }
